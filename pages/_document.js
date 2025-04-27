3f721712dcfb9fb7b6da82961af56006e694ef4f
Satwika Kadiyala <satwika.kadiyalaa@gmail.com>
2025-04-28 00:58:54 +0530

    Added missing images and files

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        return { ...initialProps }
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
     }
 
     render() {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                    <link rel="icon" href="/favicon.ico" />
+                    <meta property="og:title" content="Satwika Kadiyala Portfolio" />
+                    <meta property="og:description" content="Welcome to my professional portfolio website!" />
+                    <meta property="og:image" content="/preview.png" />
+                    <meta property="og:url" content="https://your-deployment-link.com" />
+                    <meta name="twitter:card" content="summary_large_image" />
+                </Head>
                 <body>
                     <Main />
                     <NextScript />
                 </body>
             </Html>
-        )
+        );
     }
 }
 
-export default MyDocument
+export default MyDocument;
